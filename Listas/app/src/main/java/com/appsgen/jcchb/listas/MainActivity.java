a402bfbb93df4fab446824ef7ba0df3f5549b5eb
Juan <juanchav@hotmail.com>
2014-11-20 18:52:05 -0500

    Verificar que funciona git

--- Listas/app/src/main/java/com/appsgen/jcchb/listas/MainActivity.java
+++ Listas/app/src/main/java/com/appsgen/jcchb/listas/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //este es un cambio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
